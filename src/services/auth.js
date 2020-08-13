5b40e9e087dcb5dc9f2f55708d57e0379b15b0f0
Briseida Pagador <bpagador@gmail.com>
2020-08-12 17:11:10 -0700

    added fetchLogin service

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,3 +2,7 @@ import { post } from './request';
 
 export const fetchSignup = (email, password, avatar) =>
   post('/api/v1/auth/signup', { email, password, avatar });
+
+
+export const fetchLogin = (email, password) => 
+  post('/api/v1/auth/login', { email, password });
